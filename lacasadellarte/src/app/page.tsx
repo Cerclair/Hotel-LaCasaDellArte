37502ae991be67279210f67d17efe3884f129c5a
udani lokuhetti <udani.20232492@iit.ac.lk>
2025-11-08 13:46:57 +0530

    fixed build error

--- lacasadellarte/src/app/page.tsx
+++ lacasadellarte/src/app/page.tsx
@@ -385,7 +385,7 @@ export default function Home() {
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
-                allowFullScreen=""
+                allowFullScreen={true}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
                 title="La Casa DellArte Location"
